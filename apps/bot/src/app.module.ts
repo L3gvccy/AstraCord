2e0654ea83c5d46180d465c8d315baa800d32a9d
L3gvccy <oleksandr_iv@knu.ua>
2026-04-04 21:59:32 +0300

    basic bot setup

---
+++ apps/bot/src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from "@nestjs/common";
+import { GatewayIntentBits, IntentsBitField } from "discord.js";
+import { NecordModule } from "necord";
+import "dotenv/config";
+import { AppService } from "./app.service";
+
+@Module({
+  imports: [
+    NecordModule.forRoot({
+      token: process.env.DISCORD_BOT_TOKEN ?? "",
+      intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildModeration,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildInvites,
+        GatewayIntentBits.GuildEmojisAndStickers,
+        GatewayIntentBits.MessageContent,
+      ],
+    }),
+  ],
+  controllers: [],
+  providers: [AppService],
+})
+export class AppModule {}
