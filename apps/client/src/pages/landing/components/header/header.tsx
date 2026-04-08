b1e1b845157e1ebb7b20ff4509cb2560ba4ef2f8
L3gvccy <oleksandr_iv@knu.ua>
2026-04-08 21:26:20 +0300

    typescript version update

--- apps/client/src/pages/landing/components/header/header.tsx
+++ apps/client/src/pages/landing/components/header/header.tsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header>
+    <header className="px-4">
       <div className="flex items-center gap-6 bg-slate-800 border border-slate-500 shadow-lg shadow-cyan-800/20 rounded-2xl px-4 py-2">
         <Link to="/">
           <img src="/logo.png" className="w-10" />
