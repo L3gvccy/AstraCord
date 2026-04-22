ebecd9ffa9caa222229b948f06b3b21047b0c6bd
L3gvccy <oleksandr_iv@knu.ua>
2026-04-22 16:07:52 +0300

    Add skeleton loading to dashboard

--- apps/client/src/pages/dashboard/components/index/dashboard-index.tsx
+++ apps/client/src/pages/dashboard/components/index/dashboard-index.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const DashboardIndex = () => {
   return (
-    <div className="flex flex-col gap-6 w-full h-full items-center justify-center">
-      <img src="/logo.png" className="w-44" />
-      <p className="text-6xl font-semibold bg-clip-text text-transparent bg-linear-45 to-violet-600 via-cyan-500 from-teal-600">
+    <div className="flex flex-col gap-2 w-full h-full items-center justify-center">
+      <img src="/logo.png" className="w-38 sm:w-44" />
+      <p className="text-4xl sm:text-6xl font-semibold bg-clip-text text-transparent bg-linear-45 to-violet-600 via-cyan-500 from-teal-600">
         AstraCord
       </p>
     </div>
--- apps/client/src/pages/dashboard/dashboard.tsx
+++ apps/client/src/pages/dashboard/dashboard.tsx
@@ -6,6 +6,7 @@ import { Outlet, useNavigate, useParams } from "react-router-dom";
 import { apiClient } from "@/utils/api-client";
 import { GET_GUILD_BY_ID_URL } from "@/utils/constants";
 import type { GuildType } from "@astracord/shared";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const Dashboard = () => {
   const navigate = useNavigate();
@@ -39,7 +40,9 @@ const Dashboard = () => {
     } catch (error) {
       navigate("/servers");
     } finally {
-      setLoading(false);
+      setTimeout(() => {
+        setLoading(false);
+      }, 2000);
     }
   };
 
@@ -52,10 +55,38 @@ const Dashboard = () => {
       <div className="flex h-full w-full flex-col gap-4 px-4 py-4 overflow-hidden">
         <DashboardHeader toggleSidebar={toggleSidebar} />
 
-        {loading && !guildInfo ? (
-          <div>Loading...</div>
+        {loading || !guildInfo ? (
+          <div className="relative flex flex-1 min-h-0 gap-4">
+            <div className="hidden md:flex flex-col items-center min-w-64 w-auto rounded-[20px] overflow-hidden border border-white/5 bg-linear-to-b from-blue-950 to-slate-900 p-4">
+              <div className="block h-10 w-full p-1">
+                <Skeleton className="rounded-xl w-8 h-8 bg-slate-900" />
+              </div>
+              <div className="flex flex-col gap-2 items-center mb-6">
+                <Skeleton className="rounded-full w-16 h-16 bg-slate-900" />
+                <Skeleton className="rounded-md w-33 h-7 bg-slate-900" />
+              </div>
+              <div className="flex flex-col w-full gap-1 items-center mb-6">
+                <Skeleton className="rounded-xl w-full h-9 bg-slate-900" />
+                {Array.from({ length: 6 }, (_, index) => (
+                  <Skeleton className="rounded-xl w-full h-9 ml-2 bg-slate-900" />
+                ))}
+                <Skeleton className="rounded-xl w-full h-9 bg-slate-900" />
+                {Array.from({ length: 3 }, (_, index) => (
+                  <Skeleton className="rounded-xl w-full h-9 ml-2 bg-slate-900" />
+                ))}
+                <Skeleton className="rounded-xl w-full h-9 bg-slate-900" />
+                {Array.from({ length: 4 }, (_, index) => (
+                  <Skeleton className="rounded-xl w-full h-9 ml-2 bg-slate-900" />
+                ))}
+              </div>
+            </div>
+            <div className="flex flex-col gap-2 flex-1 items-center justify-center rounded-[20px] border border-white/5 bg-linear-to-b from-blue-950 to-slate-900 p-4">
+              <Skeleton className="rounded-full w-38 sm:w-44 h-38 sm:h-44 bg-slate-900" />
+              <Skeleton className="rounded-md w-46 sm:w-76 h-10 sm:h-15 bg-slate-900" />
+            </div>
+          </div>
         ) : (
-          <div className="relative flex flex-1 min-h-0 gap-3">
+          <div className="relative flex flex-1 min-h-0 gap-4">
             <DashboardNav
               guild={guildInfo}
               isCollapsed={isSidebarCollapsed}
