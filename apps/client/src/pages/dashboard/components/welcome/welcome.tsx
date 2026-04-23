08d619657c869b2c14c4e072083b303dd30c0855
L3gvccy <oleksandr_iv@knu.ua>
2026-04-24 00:03:36 +0300

    refactor: improve layout and structure of welcome message settings

--- apps/client/src/pages/dashboard/components/welcome/welcome.tsx
+++ apps/client/src/pages/dashboard/components/welcome/welcome.tsx
@@ -60,92 +60,94 @@ const Welcome = () => {
         </DashboardOutletHeader.Description>
       </DashboardOutletHeader>
 
-      <div className="flex gap-4 items-center">
-        <p className="text-xl">Welcome message enabled</p>
-        <Switch
-          checked={config.isEnabled}
-          onCheckedChange={() => {
-            setConfig((prev) => {
-              if (!prev) return undefined;
+      <div className="flex flex-col gap-4 p-4 rounded-lg bg-slate-900">
+        <div className="flex gap-4 items-center">
+          <p className="text-xl">Welcome message enabled</p>
+          <Switch
+            checked={config.isEnabled}
+            onCheckedChange={() => {
+              setConfig((prev) => {
+                if (!prev) return undefined;
 
-              return { ...prev, isEnabled: !prev.isEnabled };
-            });
-          }}
-        />
-      </div>
+                return { ...prev, isEnabled: !prev.isEnabled };
+              });
+            }}
+          />
+        </div>
 
-      {config.isEnabled && (
-        <>
-          <div className="flex flex-col gap-2">
-            <p className="text-lg font-semibold">Channel</p>
-            <Select
-              value={config.channelId || ""}
-              onValueChange={(value) => {
-                if (!value) return;
+        {config.isEnabled && (
+          <>
+            <div className="flex flex-col gap-2">
+              <p className="text-lg font-semibold">Channel</p>
+              <Select
+                value={config.channelId || ""}
+                onValueChange={(value) => {
+                  if (!value) return;
 
-                setConfig((prev) => {
-                  if (!prev) return undefined;
-
-                  return { ...prev, channelId: value };
-                });
-              }}
-            >
-              <SelectTrigger className="w-full max-w-64 data-placeholder:text-slate-400">
-                <SelectValue placeholder="Select channel" />
-              </SelectTrigger>
-
-              <SelectContent
-                position="popper"
-                side="bottom"
-                align="start"
-                sideOffset={6}
-                className="bg-slate-950 p-1"
-              >
-                {textChannels?.map((ch) => (
-                  <SelectItem
-                    key={ch.id}
-                    value={ch.id}
-                    className="text-slate-200 focus:bg-slate-900 focus:text-white data-highlighted:bg-slate-900 data-highlighted:text-white data-[state=checked]:bg-violet-700 data-[state=checked]:text-violet-200"
-                  >
-                    <div className="flex items-center gap-2">
-                      <p className="text-lg opacity-65">#</p>
-                      <p className="text-md">{ch.name}</p>
-                    </div>
-                  </SelectItem>
-                ))}
-              </SelectContent>
-            </Select>
-          </div>
-          <div className="flex flex-col w-fit gap-2">
-            <div className="flex rounded-xl border">
-              <div
-                className={`px-4 py-2 rounded-lg cursor-pointer transition-all duration-150 ${!config.isEmbed && "bg-violet-600"}`}
-                onClick={() => {
                   setConfig((prev) => {
                     if (!prev) return undefined;
 
-                    return { ...prev, isEmbed: false };
+                    return { ...prev, channelId: value };
                   });
                 }}
               >
-                <p className="text-md">Text message</p>
-              </div>
-              <div
-                className={`px-4 py-2 rounded-lg cursor-pointer transition-all duration-150 ${config.isEmbed && "bg-violet-600"}`}
-                onClick={() => {
-                  setConfig((prev) => {
-                    if (!prev) return undefined;
+                <SelectTrigger className="w-full max-w-64 data-placeholder:text-slate-400">
+                  <SelectValue placeholder="Select channel" />
+                </SelectTrigger>
 
-                    return { ...prev, isEmbed: true };
-                  });
-                }}
-              >
-                <p className="text-md">Embed message</p>
+                <SelectContent
+                  position="popper"
+                  side="bottom"
+                  align="start"
+                  sideOffset={6}
+                  className="bg-slate-950 p-1"
+                >
+                  {textChannels?.map((ch) => (
+                    <SelectItem
+                      key={ch.id}
+                      value={ch.id}
+                      className="text-slate-200 focus:bg-slate-900 focus:text-white data-highlighted:bg-slate-900 data-highlighted:text-white data-[state=checked]:bg-violet-700 data-[state=checked]:text-violet-200"
+                    >
+                      <div className="flex items-center gap-2">
+                        <p className="text-lg opacity-65">#</p>
+                        <p className="text-md">{ch.name}</p>
+                      </div>
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </div>
+            <div className="flex flex-col w-fit gap-2">
+              <div className="flex rounded-xl border">
+                <div
+                  className={`px-4 py-2 rounded-lg cursor-pointer transition-all duration-150 ${!config.isEmbed && "bg-violet-600"}`}
+                  onClick={() => {
+                    setConfig((prev) => {
+                      if (!prev) return undefined;
+
+                      return { ...prev, isEmbed: false };
+                    });
+                  }}
+                >
+                  <p className="text-md">Text message</p>
+                </div>
+                <div
+                  className={`px-4 py-2 rounded-lg cursor-pointer transition-all duration-150 ${config.isEmbed && "bg-violet-600"}`}
+                  onClick={() => {
+                    setConfig((prev) => {
+                      if (!prev) return undefined;
+
+                      return { ...prev, isEmbed: true };
+                    });
+                  }}
+                >
+                  <p className="text-md">Embed message</p>
+                </div>
               </div>
             </div>
-          </div>
-        </>
-      )}
+          </>
+        )}
+      </div>
     </div>
   );
 };
