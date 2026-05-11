025e3196530df6e91e576a0538a3cbf22a337cc1
L3gvccy <oleksandr_iv@knu.ua>
2026-05-11 21:00:11 +0300

    fix: remove deleted module from add.module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,15 +1,21 @@
 import { Module } from "@nestjs/common";
-import { PrismaService } from './prisma/prisma.service';
-import { AuthModule } from './auth/auth.module';
-import { GuildsModule } from './guilds/guilds.module';
-import { WelcomeMessageModule } from './welcome-message/welcome-message.module';
-import { ServerStatsModule } from './server-stats/server-stats.module';
-import { JtcModule } from './jtc/jtc.module';
-import { TicketsModule } from './tickets/tickets.module';
-import { CountersModule } from './counters/counters.module';
+import { PrismaService } from "./prisma/prisma.service";
+import { AuthModule } from "./auth/auth.module";
+import { GuildsModule } from "./guilds/guilds.module";
+import { WelcomeMessageModule } from "./welcome-message/welcome-message.module";
+import { ServerStatsModule } from "./server-stats/server-stats.module";
+import { JtcModule } from "./jtc/jtc.module";
+import { TicketsModule } from "./tickets/tickets.module";
 
 @Module({
-  imports: [AuthModule, GuildsModule, WelcomeMessageModule, ServerStatsModule, JtcModule, TicketsModule, CountersModule],
+  imports: [
+    AuthModule,
+    GuildsModule,
+    WelcomeMessageModule,
+    ServerStatsModule,
+    JtcModule,
+    TicketsModule,
+  ],
   controllers: [],
   providers: [PrismaService],
 })
